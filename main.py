ed389da40706d514d34ab5ac56d86fea81a0ec98
userlg <guipeluis90@gmail.com>
2025-11-13 09:16:54 -0400

    fix : new location to the files

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import os
 from src.helpers import methods as m
 
 if __name__ == "__main__":
+    location1 = "Z:/8. Base Datos Unica"
 
-    location = "W:/4. PREPARAR RESUMEN"
+    location2 = "Z:/4. PREPARAR RESUMEN"
 
-    if os.path.exists(location):
-        m.explore_directories(location)
+    if os.path.exists(location1):
+        m.explore_directories(location1)
+
+    if os.path.exists(location2):
+        m.explore_directories(location2)
